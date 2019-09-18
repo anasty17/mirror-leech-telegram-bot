5cb5f228adec09dfe0848d30544582e2e3516bf0
jaskaranSM <37726998+jaskaranSM@users.noreply.github.com>
2019-09-18 10:46:50 -0400

    Linux Doesnt like Spaces.

--- aria.sh
+++ aria.sh
@@ -1,2 +1,2 @@
-export DOWNLOAD_DIR = "./download"
+export DOWNLOAD_DIR="./download"
 aria2c --dir $DOWNLOAD_DIR --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800  --max-connection-per-server=10 --rpc-max-request-size=1024M --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 --daemon=true --allow-overwrite=true
