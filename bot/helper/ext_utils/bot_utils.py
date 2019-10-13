7228461ab98c537369940573c92e407162ed6db7
lzzy12 <jhashivam2020@gmail.com>
2019-10-13 21:14:47 +0530

    Cast time formats to int
    Signed-off-by: lzzy12 <jhashivam2020@gmail.com>

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -95,15 +95,18 @@ def get_readable_message(progress_list: list = None):
 def get_readable_time(seconds: int) -> str:
     result = ''
     (days, remainder) = divmod(seconds, 86400)
+    days = int(days)
     if days != 0:
         result += f'{days}d'
     (hours, remainder) = divmod(remainder, 3600)
+    hours = int(hours)
     if hours != 0:
         result += f'{hours}h'
     (minutes, seconds) = divmod(remainder, 60)
+    minutes = int(minutes)
     if minutes != 0:
         result += f'{minutes}m'
-
+    seconds = int(seconds)
     result += f'{seconds}s'
     return result
 
