fa664e6663cec21cfd49e542e9f330ad853afa02
Shivam Jha <jhashivam2020@gmail.com>
2019-10-17 21:45:03 +0530

    Check if log.txt exists before trying to open
    This fixes #3

--- bot/__init__.py
+++ bot/__init__.py
@@ -5,8 +5,10 @@ import threading
 from telegram.ext import Updater
 import os
 
-with open('log.txt', 'r+') as f:
-    f.truncate(0)
+
+if os.path.exists('log.txt'):
+    with open('log.txt', 'r+') as f:
+        f.truncate(0)
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     handlers=[logging.FileHandler('log.txt'), logging.StreamHandler()],
