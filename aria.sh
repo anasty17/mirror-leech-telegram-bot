c29de8e940d3a25a97a2132b9be9c585034c21d9
magneto261290 <devsingh261298@gmail.com>
2020-06-15 14:21:42 +0530

    Fix SSL handsake error

--- aria.sh
+++ aria.sh
@@ -1,7 +1,7 @@
 export MAX_DOWNLOAD_SPEED=0
 tracker_list=$(curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk '$1' | tr '\n' ',')
 export MAX_CONCURRENT_DOWNLOADS=4
-aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
+aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 --check-certificate=false\
    --max-connection-per-server=10 --rpc-max-request-size=1024M \
    --bt-tracker="[$tracker_list]" --bt-max-peers=0 --seed-time=0.01 --min-split-size=10M \
    --follow-torrent=mem --split=10 \
