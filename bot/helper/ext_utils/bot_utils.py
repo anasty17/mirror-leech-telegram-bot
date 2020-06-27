b5a162f15d1715faa5529fa998cfcdb29f0cccbc
lzzy12 <jhashivam2020@gmail.com>
2020-06-27 23:40:15 +0530

    Use subprocess.run to call extract shell script
    Signed-off-by: lzzy12 <jhashivam2020@gmail.com>

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -137,8 +137,10 @@ def is_magnet(url: str):
         return True
     return False
 
+
 def is_mega_link(url: str):
     return "mega.nz" in url
+
 
 def new_thread(fn):
     """To use as decorator to make a function call threaded.
