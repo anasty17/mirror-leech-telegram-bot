ddca28b184b0ea7e6868889622b4058c5493c586
EdithX <71973948+EdithX@users.noreply.github.com>
2021-10-06 09:56:10 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -29,6 +29,7 @@ from bot.helper.mirror_utils.download_utils.telegram_downloader import TelegramD
 from bot.helper.mirror_utils.status_utils import listeners
 from bot.helper.mirror_utils.status_utils.extract_status import ExtractStatus
 from bot.helper.mirror_utils.status_utils.tar_status import TarStatus
+from bot.helper.mirror_utils.status_utils.clone_status import CloneStatus
 from bot.helper.mirror_utils.status_utils.split_status import SplitStatus
 from bot.helper.mirror_utils.status_utils.upload_status import UploadStatus
 from bot.helper.mirror_utils.status_utils.tg_upload_status import TgUploadStatus
@@ -38,6 +39,9 @@ from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.message_utils import *
 from bot.helper.telegram_helper import button_build
+
+from bot import dispatcher, LOGGER, CLONE_LIMIT, STOP_DUPLICATE
+from bot.helper.ext_utils.bot_utils import get_readable_file_size, check_limit
 
 ariaDlManager = AriaDownloadHelper()
 ariaDlManager.start_listener()
@@ -427,8 +431,65 @@ def _mirror(bot, update, isTar=False, extract=False, isZip=False, isQbit=False, 
 
     if bot_utils.is_gdrive_link(link):
         if not isTar and not extract and not isLeech:
-            sendMessage(f"Use /{BotCommands.CloneCommand} to clone Google Drive file/folder\nUse /{BotCommands.TarMirrorCommand} to make tar of Google Drive folder\nUse /{BotCommands.UnzipMirrorCommand} to extracts archive Google Drive file", bot, update)
-            return
+            args = update.message.text.split(" ", maxsplit=1)
+            if len(args) > 1:
+                link = args[1]
+                gd = gdriveTools.GoogleDriveHelper()
+                res, size, name, files = gd.helper(link)
+                if res != "":
+                    sendMessage(res, context.bot, update)
+                    return
+                if STOP_DUPLICATE:
+                    LOGGER.info('Checking File/Folder if already in Drive...')
+                    smsg, button = gd.drive_list(name, True, True)
+                    if smsg:
+                        msg3 = "File/Folder is already available in Drive.\nHere are the search results:"
+                        sendMarkup(msg3, context.bot, update, button)
+                        return
+                if CLONE_LIMIT is not None:
+                    result = check_limit(size, CLONE_LIMIT)
+                    if result:
+                        msg2 = f'Failed, Clone limit is {CLONE_LIMIT}.\nYour File/Folder size is {get_readable_file_size(size)}.'
+                        sendMessage(msg2, context.bot, update)
+                        return
+                if files < 15:
+                    msg = sendMessage(f"Cloning: <code>{link}</code>", context.bot, update)
+                    result, button = gd.clone(link)
+                    deleteMessage(context.bot, msg)
+                else:
+                    drive = gdriveTools.GoogleDriveHelper(name)
+                    gid = ''.join(random.SystemRandom().choices(string.ascii_letters + string.digits, k=12))
+                    clone_status = CloneStatus(drive, size, update, gid)
+                    with download_dict_lock:
+                        download_dict[update.message.message_id] = clone_status
+                   sendStatusMessage(update, context.bot)
+                   result, button = drive.clone(link)
+                   with download_dict_lock:
+                       del download_dict[update.message.message_id]
+                       count = len(download_dict)
+                   try:
+                       if count == 0:
+                           Interval[0].cancel()
+                           del Interval[0]
+                           delete_all_messages()
+                       else:
+                           update_all_messages()
+                   except IndexError:
+                       pass
+               if update.message.from_user.username:
+                   uname = f'@{update.message.from_user.username}'
+               else:
+                   uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
+               if uname is not None:
+                   cc = f'\n\n<b>cc: </b>{uname}'
+                   men = f'{uname} '
+               if button in ["cancelled", ""]:
+                   sendMessage(men + result, context.bot, update)
+               else:
+                   sendMarkup(result + cc, context.bot, update, button)
+           else:
+               sendMessage('Provide G-Drive Shareable Link to Clone.', context.bot, update)
+               return
         res, size, name, files = gdriveTools.GoogleDriveHelper().helper(link)
         if res != "":
             sendMessage(res, bot, update)
