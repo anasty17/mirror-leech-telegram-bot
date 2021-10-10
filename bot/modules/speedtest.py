97b04de4cf3fde0bc21ab0beadc0d7d138f6160c
yamraaj007 <69627019+crazy-racer@users.noreply.github.com>
2021-10-10 15:31:33 +0530

    Update speedtest.py

--- bot/modules/speedtest.py
+++ bot/modules/speedtest.py
@@ -15,17 +15,17 @@ def speedtest(update, context):
     test.results.share()
     result = test.results.dict()
     string_speed = f'''
-<b>Server</b>
-<b>Name:</b> <code>{result['server']['name']}</code>
-<b>Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
-<b>Sponsor:</b> <code>{result['server']['sponsor']}</code>
-<b>ISP:</b> <code>{result['client']['isp']}</code>
+<b>🖥️ Server</b>
+<b>💳 Name:</b> <code>{result['server']['name']}</code>
+<b>⛳️ Country:</b> <code>{result['server']['country']}, {result['server']['cc']}</code>
+<b>💰 Sponsor:</b> <code>{result['server']['sponsor']}</code>
+<b>🏬 ISP:</b> <code>{result['client']['isp']}</code>
 
-<b>SpeedTest Results</b>
-<b>Upload:</b> <code>{speed_convert(result['upload'] / 8)}</code>
-<b>Download:</b>  <code>{speed_convert(result['download'] / 8)}</code>
-<b>Ping:</b> <code>{result['ping']} ms</code>
-<b>ISP Rating:</b> <code>{result['client']['isprating']}</code>
+<b>✈️ SpeedTest Results</b>
+<b>🔺 Upload:</b> <code>{speed_convert(result['upload'] / 8)}</code>
+<b>🔻 Download:</b>  <code>{speed_convert(result['download'] / 8)}</code>
+<b>📶 Ping:</b> <code>{result['ping']} ms</code>
+<b>🏬 ISP Rating:</b> <code>{result['client']['isprating']}</code>
 '''
     editMessage(string_speed, speed)
 
