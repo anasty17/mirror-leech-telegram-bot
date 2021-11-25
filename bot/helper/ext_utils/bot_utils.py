86f26dc16e54298cade5c7435baa7e7591b50541
Anas <77075674+anasty17@users.noreply.github.com>
2021-11-25 17:23:57 +0200

    Minor

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -228,7 +228,7 @@ def is_gdrive_link(url: str):
     return "drive.google.com" in url
 
 def is_gdtot_link(url: str):
-    url = re.match(r'https?://.*.gdtot.\S+', url)
+    url = re.match(r'https?://.*\.gdtot\.\S+', url)
     return bool(url)
 
 def is_mega_link(url: str):
