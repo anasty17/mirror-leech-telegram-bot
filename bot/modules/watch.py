714defdbf08deee3a2b919671749642eb5440f38
Anas <77075674+anasty17@users.noreply.github.com>
2021-12-03 08:00:42 +0200

    Update watch.py

--- bot/modules/watch.py
+++ bot/modules/watch.py
@@ -51,8 +51,8 @@ def _watch(bot, update, isZip=False, isLeech=False, pswd=None):
     if not is_url(link):
         help_msg = "<b>Send link along with command line:</b>"
         help_msg += "\n<code>/command</code> {link} |newname pswd: mypassword [𝚣𝚒𝚙]"
-        help_msg += "\n\n<b>By Replying to link:</b>"
-        help_msg ×= "\n<code>/command</code> |newname pswd: mypassword [𝚣𝚒𝚙]"
+        help_msg += "\n\n<b>By replying to link:</b>"
+        help_msg += "\n<code>/command</code> |newname pswd: mypassword [𝚣𝚒𝚙]"
         return sendMessage(help_msg, bot, update)
 
     listener = MirrorListener(bot, update, isZip, isLeech=isLeech, pswd=pswd)
