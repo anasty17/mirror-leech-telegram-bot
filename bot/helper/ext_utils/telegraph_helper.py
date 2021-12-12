db74123ed810af7c79b20582bc93507eb1343856
Sαƚყαɱ <69762426+thesatyamxyz@users.noreply.github.com>
2021-12-12 21:27:36 +0530

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -57,4 +57,4 @@ class TelegraphHelper:
 			return self.edit_page(path, title, content)
 
 
-telegraph=TelegraphHelper('Mirror-Leech-Telegram-Bot', 'https://github.com/anasty17/mirror-leech-telegram-bot')
+telegraph=TelegraphHelper('The-Satyam', 'https://telegram.dog/thesatyamxyz')
