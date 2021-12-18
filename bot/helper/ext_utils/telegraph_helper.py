87c4ef1a833ce8a6e31e119c5afafdf48e8a8d78
Relavavik <89246053+Relavavik@users.noreply.github.com>
2021-12-18 17:52:43 +0600

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -57,4 +57,4 @@ class TelegraphHelper:
 			return self.edit_page(path, title, content)
 
 
-telegraph=TelegraphHelper('Mirror-Leech-Telegram-Bot', 'https://github.com/anasty17/mirror-leech-telegram-bot')
+telegraph=TelegraphHelper('Relavavik Mirror', 'https://t.me/Relavavik')
