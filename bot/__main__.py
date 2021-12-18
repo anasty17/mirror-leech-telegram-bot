a65d274e1354a698cde6b3bccd81b588deeb661d
Relavavik <89246053+Relavavik@users.noreply.github.com>
2021-12-18 17:31:07 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -62,8 +62,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+MwgSi5vmQEA2N2Vk")
+    buttons.buildbutton("Owner", "https://t.me/Relavavik")
+    buttons.buildbutton("Owner Channel", "https://t.me/Relavavikx")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
