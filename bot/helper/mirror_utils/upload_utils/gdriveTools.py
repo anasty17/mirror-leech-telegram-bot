89c9a79c1065bafe17dbeebf3cfd633333979abf
Relavavik <89246053+Relavavik@users.noreply.github.com>
2021-12-23 18:20:57 +0600

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -170,7 +170,7 @@ class GoogleDriveHelper:
         # File body description
         file_metadata = {
             'name': file_name,
-            'description': 'Uploaded by Mirror-leech-telegram-bot',
+            'description': 'Uploaded by Relavavik',
             'mimeType': mime_type,
         }
         if parent_id is not None:
