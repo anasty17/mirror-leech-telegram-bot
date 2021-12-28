65aa89261e01b052591ef62b3418637a4173b171
vmxone <96577103+vmxone@users.noreply.github.com>
2021-12-28 19:25:31 +0800

    Update speedtest.py

--- bot/modules/speedtest.py
+++ bot/modules/speedtest.py
@@ -7,7 +7,7 @@ from telegram.ext import CommandHandler
 
 
 def speedtest(update, context):
-    speed = sendMessage("Running Speed Test . . . ", context.bot, update)
+    speed = sendMessage("Menjalankan Test . . . ", context.bot, update)
     test = Speedtest()
     test.get_best_server()
     test.download()
