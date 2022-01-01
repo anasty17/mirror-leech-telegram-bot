addad62da51192bb83cf69b33515f2d273465eb8
anasty17 <e.anastayyar@gmail.com>
2022-01-01 19:44:53 +0200

    minor fix in clone
    Signed-off-by: anasty17 <e.anastayyar@gmail.com>

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -83,7 +83,7 @@ def cloneNode(update, context):
                     update_all_messages()
             except IndexError:
                 pass
-            cc = f'\n\n<b>cc: </b>{tag}'
+        cc = f'\n\n<b>cc: </b>{tag}'
         if button in ["cancelled", ""]:
             sendMessage(f"{tag} {result}", context.bot, update)
         else:
