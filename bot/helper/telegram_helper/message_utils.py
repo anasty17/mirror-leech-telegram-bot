b7e8b6143293f8928a9b58a6fc65b5187629994b
rahul112kapoor <68430011+rahul112kapoor@users.noreply.github.com>
2022-01-03 18:12:10 +0530

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -1,13 +1,13 @@
 import time
 
-from telegram import InlineKeyboardMarkup
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.message import Message
 from telegram.update import Update
 from telegram.error import RetryAfter
 from pyrogram.errors import FloodWait
 
 from bot import AUTO_DELETE_MESSAGE_DURATION, LOGGER, status_reply_dict, status_reply_dict_lock, \
-                Interval, DOWNLOAD_STATUS_UPDATE_INTERVAL, RSS_CHAT_ID, rss_session, bot
+                Interval, DOWNLOAD_STATUS_UPDATE_INTERVAL, RSS_CHAT_ID, rss_session, bot, LOG_CHANNEL
 from bot.helper.ext_utils.bot_utils import get_readable_message, setInterval
 
 
@@ -37,6 +37,13 @@ def sendMarkup(text: str, bot, update: Update, reply_markup: InlineKeyboardMarku
     except Exception as e:
         LOGGER.error(str(e))
         return
+
+def sendLog(text: str, bot, update: Update, reply_markup: InlineKeyboardMarkup):
+    try:
+        return bot.send_message(f"{LOG_CHANNEL}",
+                             text=text, disable_web_page_preview=True, reply_markup=reply_markup, allow_sending_without_reply=True, parse_mode='HTMl')
+    except Exception as e:
+        LOGGER.error(str(e))
 
 def editMessage(text: str, message: Message, reply_markup=None):
     try:
