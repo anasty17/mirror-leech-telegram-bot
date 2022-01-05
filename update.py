1160468414c028468858dc52e6015c63c5cfa27d
Sathishzus <73419683+Sathishzus@users.noreply.github.com>
2022-01-05 21:21:31 +0530

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except TypeError:
-    UPSTREAM_REPO = "https://github.com/FOX-X-BOT/mirror-leech-telegram-bot/tree/master"
+    UPSTREAM_REPO = "https://github.com/FOX-X-BOT/mirror-leech-telegram-bot"
 
 if os.path.exists('.git'):
     subprocess.run(["rm", "-rf", ".git"])
