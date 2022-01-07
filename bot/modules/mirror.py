c3587faa7a6d1b550564df5e39e5cac1c5ab5ec4
Kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2022-01-07 20:13:12 +0530

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -272,7 +272,7 @@ class MirrorListener:
                     pass
                 del download_dict[self.uid]
                 count = len(download_dict)
-            log_msg = f"\n\n══════════════════════════\n\n<b>Donwload Owner {self.tag}</b>\n\n"
+            log_msg = f"\n\n<b>════════════ @KristyCloud ═════════════</b>\n\n"
             logmsg = sendLog(log_msg + msg , self.bot, self.update, InlineKeyboardMarkup(buttons.build_menu(2)))
             if logmsg:
                 log_msg = f"\n\n══════════════════════════\n\n<b>Your File has been Successfully Uploaded, Click Below Button to get Download Links.</b>"
