8f1460bcb09887e1622bbc24f728becb147637b8
Kristy-offl <81421572+kristy-offl@users.noreply.github.com>
2022-01-10 20:16:44 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -91,7 +91,7 @@ def cloneNode(update, context):
             log_msg = f"<b>═══════ @KristyCloud ═══════</b>\n\n"
             logmsg = sendLog(log_msg + result , context.bot, update, button)
             if logmsg:
-                log_msg = f"\n\nHello {tag} \n\n Your Cloned File Uploaded\n\n══════════════════════════\n\n<b>Your File has been Successfully Uploaded, Click Below Button to get Download Links.👇</b>"
+                log_msg = f"\n\n<b>Hello {tag}\n\nYour File has been Successfully Uploaded, Click Below Button to get Download Links.👇</b>"
                 sendMarkup(log_msg, context.bot, update, InlineKeyboardMarkup([[InlineKeyboardButton(text="Click Here 🔗", url=logmsg.link)]]))
         if gdtot_link:
             gd.deletefile(link)
