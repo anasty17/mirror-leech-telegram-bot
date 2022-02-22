454d7c14d9584a177a413e1bdbe83d32fae7b010
P R I Y O <84788291+itspriyo@users.noreply.github.com>
2022-02-22 17:13:42 +0600

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except TypeError:
-    UPSTREAM_REPO = "https://github.com/anasty17/mirror-leech-telegram-bot"
+    UPSTREAM_REPO = "https://github.com/itspriyo/priyo_mirror"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
