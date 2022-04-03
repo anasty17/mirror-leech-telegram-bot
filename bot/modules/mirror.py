175b878b93e14c59d7f4bb042edd4bb35f49fbc7
Anas <77075674+anasty17@users.noreply.github.com>
2022-04-04 01:15:42 +0300

    To avoid flooding
    Bot can only send 20 message per minute

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -442,7 +442,7 @@ def _mirror(bot, message, isZip=False, extract=False, isQbit=False, isLeech=Fals
         Thread(target=add_aria2c_download, args=(link, f'{DOWNLOAD_DIR}{listener.uid}', listener, name)).start()
 
     if multi > 1:
-        sleep(3)
+        sleep(7)
         nextmsg = type('nextmsg', (object, ), {'chat_id': message.chat_id, 'message_id': message.reply_to_message.message_id + 1})
         nextmsg = sendMessage(message_args[0], bot, nextmsg)
         multi -= 1
