b3ce30e2707a32309e339fcc4337ba106423ccd2
Anas <77075674+anasty17@users.noreply.github.com>
2022-04-27 22:28:42 +0300

    Fix missing arg in user string session gen.

--- generate_string_session.py
+++ generate_string_session.py
@@ -2,5 +2,5 @@ from pyrogram import Client
 
 API_KEY = int(input("Enter API KEY: "))
 API_HASH = input("Enter API HASH: ")
-with Client(api_id=API_KEY, api_hash=API_HASH, in_memory=True) as app:
+with Client(name='USS', api_id=API_KEY, api_hash=API_HASH, in_memory=True) as app:
     print(app.export_session_string())
