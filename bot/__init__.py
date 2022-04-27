6797ab91fa32b773cb0b7018423d2375620abff2
NOOBie <44431105+sinoobie@users.noreply.github.com>
2022-04-27 11:15:11 +0700

    EXTENTION_FILTER typo
    only add EXTENTION_FILTER config if not null

--- bot/__init__.py
+++ bot/__init__.py
@@ -141,8 +141,9 @@ except:
 try:
     fx = getConfig('EXTENTION_FILTER')
     fx = fx.split(' ')
-    for x in fx:
-        EXTENTION_FILTER.add(x.lower())
+    if fx != '':
+        for x in fx:
+            EXTENTION_FILTER.add(x.lower())
 except:
     pass
 try:
