f1621c7d49ff0779d604a8562a9c3470b6a9de91
UserLazy <78595134+UserLazy@users.noreply.github.com>
2022-05-20 13:26:21 +0700

    add ParseMode

--- bot/modules/shell.py
+++ bot/modules/shell.py
@@ -1,4 +1,5 @@
-from subprocess import run as srun
+import subprocess
+from telegram import ParseMode
 from telegram.ext import CommandHandler
 
 from bot import LOGGER, dispatcher
@@ -9,34 +10,43 @@ from bot.helper.telegram_helper.message_utils import sendMessage
 
 def shell(update, context):
     message = update.effective_message
-    cmd = message.text.split(' ', 1)
+    cmd = message.text.split(" ", 1)
     if len(cmd) == 1:
-        return sendMessage('No command to execute was given.', context.bot, update.message)
+        message.reply_text(
+            "No command to execute was given.", allow_sending_without_reply=True
+        )
+        return
     cmd = cmd[1]
-    process = srun(cmd, capture_output=True, shell=True)
-    reply = ''
-    stderr = process.stderr.decode('utf-8')
-    stdout = process.stdout.decode('utf-8')
-    if len(stdout) != 0:
-        reply += f"*Stdout*\n<code>{stdout}</code>\n"
+    process = subprocess.Popen(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        shell=True,
+    )
+    stdout, stderr = process.communicate()
+    reply = ""
+    stderr = stderr.decode()
+    stdout = stdout.decode()
+    if stdout:
+        reply += f"*Stdout*\n`{stdout}`\n"
         LOGGER.info(f"Shell - {cmd} - {stdout}")
-    if len(stderr) != 0:
-        reply += f"*Stderr*\n<code>{stderr}</code>\n"
+    if stderr:
+        reply += f"*Stderr*\n`{stderr}`\n"
         LOGGER.error(f"Shell - {cmd} - {stderr}")
     if len(reply) > 3000:
-        with open('shell_output.txt', 'w') as file:
+        with open("shell_output.txt", "w") as file:
             file.write(reply)
-        with open('shell_output.txt', 'rb') as doc:
+        with open("shell_output.txt", "rb") as doc:
             context.bot.send_document(
                 document=doc,
                 filename=doc.name,
                 reply_to_message_id=message.message_id,
-                chat_id=message.chat_id)
-    elif len(reply) != 0:
-        sendMessage(reply, context.bot, update.message)
+                chat_id=message.chat_id,
+            )
     else:
-        sendMessage('No Reply', context.bot, update.message)
-
+        message.reply_text(
+            reply, parse_mode=ParseMode.MARKDOWN, allow_sending_without_reply=True
+        )
 
 SHELL_HANDLER = CommandHandler(BotCommands.ShellCommand, shell,
                                                   filters=CustomFilters.owner_filter, run_async=True)
