468d18308a1ffb4b138f2985aea3e5a65cd70b12
Dragon-1237 <89452912+Dragon-1237@users.noreply.github.com>
2022-05-31 08:46:58 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,6 +1,5 @@
 from logging import getLogger, FileHandler, StreamHandler, INFO, basicConfig, error as log_error, info as log_info, warning as log_warning
 from socket import setdefaulttimeout
-import os
 from faulthandler import enable as faulthandler_enable
 from telegram.ext import Updater as tgUpdater
 from qbittorrentapi import Client as qbClient
@@ -30,15 +29,6 @@ load_dotenv('config.env', override=True)
 def getConfig(name: str):
     return environ[name]
 
-CONFIG_FILE_URL = os.environ.get('CONFIG_FILE_URL', None)
-if CONFIG_FILE_URL is not None:
-    res = requests.get(CONFIG_FILE_URL)
-    if res.status_code == 200:
-        with open('config.env', 'wb+') as f:
-            f.write(res.content)
-            f.close()
-    else:
-        logging.error(res.status_code)
         
 try:
     NETRC_URL = getConfig('NETRC_URL')
