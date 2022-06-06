b523958a0df408b1a049bd4affa522a7300e133c
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-06-06 23:18:33 +0530

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -10,8 +10,8 @@ from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.mirror_utils.status_utils.clone_status import CloneStatus
 from bot import dispatcher, LOGGER, CLONE_LIMIT, STOP_DUPLICATE, download_dict, download_dict_lock, Interval
-from bot.helper.ext_utils.bot_utils import get_readable_file_size, is_gdrive_link, is_gdtot_link, new_thread
-from bot.helper.mirror_utils.download_utils.direct_link_generator import gdtot
+from bot.helper.ext_utils.bot_utils import get_readable_file_size, is_gdrive_link, is_gdtot_link, new_thread, is_appdrive_link, is_sharer_link, is_gp_link
+from bot.helper.mirror_utils.download_utils.direct_link_generator import gdtot, appdrive_dl, sharer_pw_dl, gplinks
 from bot.helper.ext_utils.exceptions import DirectDownloadLinkException
 
 
