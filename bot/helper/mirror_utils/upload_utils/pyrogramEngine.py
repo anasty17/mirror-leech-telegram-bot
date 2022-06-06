9334436005e7c4c9ec12d2223f0c23d1ba9e0016
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-06-06 22:25:26 +0530

    Update pyrogramEngine.py

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -65,13 +65,13 @@ class TgUploader:
 
     def __upload_file(self, up_path, file_, dirpath):
         if CUSTOM_FILENAME is not None:
-            cap_mono = f"{CUSTOM_FILENAME} <code>{file_}</code>"
+            cap_mono = f"{CUSTOM_FILENAME} <b>{file_}</b>"
             file_ = f"{CUSTOM_FILENAME} {file_}"
             new_path = ospath.join(dirpath, file_)
             osrename(up_path, new_path)
             up_path = new_path
         else:
-            cap_mono = f"<code>{file_}</code>"
+            cap_mono = f"<b>{file_}</b>"
         notMedia = False
         thumb = self.__thumb
         self.__is_corrupted = False
