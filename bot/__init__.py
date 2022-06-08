61d9b9999f4a07df7746eeeb450ed5a1519f60e9
mltb <e.anastayyar@gmail.com>
2022-06-08 23:56:40 +0300

    minor
    Signed-off-by: mltb <e.anastayyar@gmail.com>

--- bot/__init__.py
+++ bot/__init__.py
@@ -70,7 +70,7 @@ if TORRENT_TIMEOUT is not None:
     with open("a2c.conf", "a+") as a:
         a.write(f"bt-stop-timeout={TORRENT_TIMEOUT}\n")
 with open("a2c.conf", "a+") as a:
-    a.write(f"bt-tracker={trackers}")
+    a.write(f"bt-tracker=[{trackers}]")
 srun(["extra-api", "--conf-path=/usr/src/app/a2c.conf"])
 alive = Popen(["python3", "alive.py"])
 sleep(0.5)
