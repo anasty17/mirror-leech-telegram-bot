bb96c0f8496f804802ad297d603bbcbfdc0e8681
Romeo <92579700+JohnWickKeanue@users.noreply.github.com>
2022-06-09 11:55:58 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -65,8 +65,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+PRRzqHd31XY3ZWZk")
+    buttons.buildbutton("Repo", "https://github.com/JohnWickKeanue/mirror-leech-telegram-bot-2")
+    buttons.buildbutton("Report Group", "https://t.me/anymodapps")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
