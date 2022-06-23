0ed256b874ef3e5a3b4cc75b96698d1ceb99725d
Dawn India <61337422+Dawn-India@users.noreply.github.com>
2022-06-24 03:05:43 +0530

    Update update.py

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/shuvam-dawn/MLTB-Custom-UI"
+    UPSTREAM_REPO = "https://github.com/Dawn-India/Z-Mirror"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
