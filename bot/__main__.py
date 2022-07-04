244d8c5fde3cdf0dbca69e06c023050d5ed6f5e2
Msjdleech <106671274+Msjdleech@users.noreply.github.com>
2022-07-04 12:28:30 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -65,8 +65,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+PRRzqHd31XY3ZWZk")
+    buttons.buildbutton("Master", "@STThamizhan")
+    buttons.buildbutton("Report Group", "https://t.me/+boSh6SyewI5kOTRl")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
