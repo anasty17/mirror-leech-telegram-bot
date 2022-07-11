2e26f6bfdb868c8f2b9198d406b405e88563491f
Teamias09 <108916633+Teamias09@users.noreply.github.com>
2022-07-11 18:40:57 +0530

    Update __main__.py
    New update

--- bot/__main__.py
+++ bot/__main__.py
@@ -65,8 +65,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Repo", "https://www.github.com/anasty17/mirror-leech-telegram-bot")
-    buttons.buildbutton("Report Group", "https://t.me/+PRRzqHd31XY3ZWZk")
+    buttons.buildbutton("Join", "https://t.me/Tele_Universes")
+    buttons.buildbutton("Contact", "https://t.me/TM_TELEVERSE_Bot")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
