99e34a7e8bd0c747ded30f798f400bd54c90fd03
Juned KH <kjuned007@gmail.com>
2022-07-16 19:59:46 +0530

    split status - split show progress - remove unused imports

--- bot/helper/mirror_utils/status_utils/split_status.py
+++ bot/helper/mirror_utils/status_utils/split_status.py
@@ -1,5 +1,8 @@
-from bot import LOGGER
-from bot.helper.ext_utils.bot_utils import get_readable_file_size, MirrorStatus
+from time import time
+
+from bot import DOWNLOAD_DIR, LOGGER
+from bot.helper.ext_utils.bot_utils import get_readable_file_size, MirrorStatus, get_readable_time
+from bot.helper.ext_utils.fs_utils import get_path_size
 
 
 class SplitStatus:
@@ -8,31 +11,49 @@ class SplitStatus:
         self.__gid = gid
         self.__size = size
         self.__listener = listener
+        self.__uid = listener.uid
+        self.__start_time = time()
         self.message = listener.message
 
     def gid(self):
         return self.__gid
 
+    def speed_raw(self):
+        return self.processed_bytes() / (time() - self.__start_time)
+
+    def progress_raw(self):
+        try:
+            return self.processed_bytes() / self.__size * 100
+        except:
+            return 0
+
     def progress(self):
-        return '0'
+        return f'{round(self.progress_raw(), 2)}%'
 
     def speed(self):
-        return '0'
+        return f'{get_readable_file_size(self.speed_raw())}/s'
 
     def name(self):
         return self.__name
+
+    def size_raw(self):
+        return self.__size
 
     def size(self):
         return get_readable_file_size(self.__size)
 
     def eta(self):
-        return '0s'
+        try:
+            seconds = (self.size_raw() - self.processed_bytes()) / self.speed_raw()
+            return f'{get_readable_time(seconds)}'
+        except:
+            return '-'
 
     def status(self):
         return MirrorStatus.STATUS_SPLITTING
 
     def processed_bytes(self):
-        return 0
+        return get_path_size(f"{DOWNLOAD_DIR}{self.__uid}") - self.__size
 
     def download(self):
         return self
--- bot/helper/mirror_utils/status_utils/tg_upload_status.py
+++ bot/helper/mirror_utils/status_utils/tg_upload_status.py
@@ -1,5 +1,4 @@
 from bot.helper.ext_utils.bot_utils import MirrorStatus, get_readable_file_size, get_readable_time
-from bot import DOWNLOAD_DIR
 
 
 class TgUploadStatus:
