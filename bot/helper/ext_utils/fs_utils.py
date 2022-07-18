7e0a2721d4db3e065cb32fcab98fd81b98f90790
anasty17 <e.anastayyar@gmail.com>
2022-07-18 18:43:33 +0300

    Add support for tg premium users
    Signed-off-by: anasty17 <e.anastayyar@gmail.com>

--- bot/helper/ext_utils/fs_utils.py
+++ bot/helper/ext_utils/fs_utils.py
@@ -10,7 +10,12 @@ from math import ceil
 from re import split as re_split, I
 
 from .exceptions import NotSupportedExtractionArchive
-from bot import aria2, app, LOGGER, DOWNLOAD_DIR, get_client, TG_SPLIT_SIZE, EQUAL_SPLITS
+from bot import aria2, app, LOGGER, DOWNLOAD_DIR, get_client, TG_SPLIT_SIZE, EQUAL_SPLITS, IS_PREMIUM_USER
+
+if IS_PREMIUM_USER:
+    MAX_SPLIT_SIZE = 4194304000
+else:
+    MAX_SPLIT_SIZE = 2097152000
 
 VIDEO_SUFFIXES = ("M4V", "MP4", "MOV", "FLV", "WMV", "3GP", "MPG", "WEBM", "MKV", "AVI")
 
@@ -127,8 +132,8 @@ def split_file(path, size, file_, dirpath, split_size, listener, start_time=0, i
             if listener.split_proc.returncode == -9:
                 return False
             out_size = get_path_size(out_path)
-            if out_size > 2097152000:
-                dif = out_size - 2097152000
+            if out_size > MAX_SPLIT_SIZE:
+                dif = out_size - MAX_SPLIT_SIZE
                 split_size = split_size - dif + 5000000
                 osremove(out_path)
                 return split_file(path, size, file_, dirpath, split_size, listener, start_time, i, True)
