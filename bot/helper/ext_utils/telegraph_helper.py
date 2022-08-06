75f5ab7daa30d6ecb8fac152dd703cd7a321ad71
ADIB AKHTAB <66148227+MaxxRider@users.noreply.github.com>
2022-08-06 10:36:55 +0530

    Update telegraph_helper.py
    Telegraph.ph new domain
    graph.org

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -61,14 +61,14 @@ class TelegraphHelper:
         num_of_path = len(path)
         for content in telegraph_content :
             if nxt_page == 1 :
-                content += f'<b><a href="https://telegra.ph/{path[nxt_page]}">Next</a></b>'
+                content += f'<b><a href="https://graph.org/{path[nxt_page]}">Next</a></b>'
                 nxt_page += 1
             else :
                 if prev_page <= num_of_path:
-                    content += f'<b><a href="https://telegra.ph/{path[prev_page]}">Prev</a></b>'
+                    content += f'<b><a href="https://graph.org/{path[prev_page]}">Prev</a></b>'
                     prev_page += 1
                 if nxt_page < num_of_path:
-                    content += f'<b> | <a href="https://telegra.ph/{path[nxt_page]}">Next</a></b>'
+                    content += f'<b> | <a href="https://graph.org/{path[nxt_page]}">Next</a></b>'
                     nxt_page += 1
             self.edit_page(
                 path = path[prev_page],
