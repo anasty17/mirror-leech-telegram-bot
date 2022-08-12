627fdb81ea26042cb89e733d91bfdfbc27ed632d
anasty17 <e.anastayyar@gmail.com>
2022-08-12 19:49:23 +0300

    qbittorrent improvements
    Signed-off-by: anasty17 <e.anastayyar@gmail.com>

--- bot/modules/bt_select.py
+++ bot/modules/bt_select.py
@@ -1,5 +1,5 @@
 from telegram.ext import CommandHandler, CallbackQueryHandler
-from os import remove
+from os import remove, path as ospath
 
 from bot import aria2, BASE_URL, download_dict, dispatcher, download_dict_lock, SUDO_USERS, OWNER_ID
 from bot.helper.telegram_helper.bot_commands import BotCommands
@@ -78,11 +78,24 @@ def get_confirm(update, context):
         query.answer()
         id_ = data[3]
         if len(id_) > 20:
-            dl.client().torrents_resume(torrent_hashes=id_)
+            client = dl.client()
+            tor_info = client.torrents_info(torrent_hash=id_)[0]
+            path = tor_info.content_path.rsplit('/', 1)[0]
+            res = client.torrents_files(torrent_hash=id_)
+            for f in res:
+                if f.priority == 0:
+                    f_paths = [f"{path}/{f.name}", f"{path}/{f.name}.!qB"]
+                    for f_path in f_paths:
+                       if ospath.exists(f_path):
+                           try:
+                               remove(f_path)
+                           except:
+                               pass
+            client.torrents_resume(torrent_hashes=id_)
         else:
             res = aria2.client.get_files(id_)
             for f in res:
-                if f['selected'] == 'false':
+                if f['selected'] == 'false' and ospath.exists(f['path']):
                     try:
                         remove(f['path'])
                     except:
