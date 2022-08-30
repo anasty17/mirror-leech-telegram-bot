f3faaabfa10e19c9150290fc6980be4778e7a43c
mukto681 <112470767+mukto681@users.noreply.github.com>
2022-08-30 21:18:26 +0300

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -26,21 +26,21 @@ def stats(update, context):
     swap = swap_memory()
     memory = virtual_memory()
     stats = f'<b>Commit Date:</b> {last_commit}\n\n'\
-            f'<b>Bot Uptime:</b> {get_readable_time(time() - botStartTime)}\n'\
-            f'<b>OS Uptime:</b> {get_readable_time(time() - boot_time())}\n\n'\
-            f'<b>Total Disk Space:</b> {get_readable_file_size(total)}\n'\
-            f'<b>Used:</b> {get_readable_file_size(used)} | <b>Free:</b> {get_readable_file_size(free)}\n\n'\
-            f'<b>Upload:</b> {get_readable_file_size(net_io_counters().bytes_sent)}\n'\
-            f'<b>Download:</b> {get_readable_file_size(net_io_counters().bytes_recv)}\n\n'\
-            f'<b>CPU:</b> {cpu_percent(interval=0.5)}%\n'\
-            f'<b>RAM:</b> {memory.percent}%\n'\
-            f'<b>DISK:</b> {disk}%\n\n'\
-            f'<b>Physical Cores:</b> {cpu_count(logical=False)}\n'\
-            f'<b>Total Cores:</b> {cpu_count(logical=True)}\n\n'\
-            f'<b>SWAP:</b> {get_readable_file_size(swap.total)} | <b>Used:</b> {swap.percent}%\n'\
-            f'<b>Memory Total:</b> {get_readable_file_size(memory.total)}\n'\
-            f'<b>Memory Free:</b> {get_readable_file_size(memory.available)}\n'\
-            f'<b>Memory Used:</b> {get_readable_file_size(memory.used)}\n'
+            f'<b>⏳Bot Uptime:</b> {get_readable_time(time() - botStartTime)}\n'\
+            f'<b>🕛OS Uptime:</b> {get_readable_time(time() - boot_time())}\n\n'\
+            f'<b>💾Total Disk Space:</b> {get_readable_file_size(total)}\n'\
+            f'<b>💽Used:</b> {get_readable_file_size(used)} | <b>Free:</b> {get_readable_file_size(free)}\n\n'\
+            f'<b>⏫Upload:</b> {get_readable_file_size(net_io_counters().bytes_sent)}\n'\
+            f'<b>⏬Download:</b> {get_readable_file_size(net_io_counters().bytes_recv)}\n\n'\
+            f'<b>💻CPU:</b> {cpu_percent(interval=0.5)}%\n'\
+            f'<b>🔋RAM:</b> {memory.percent}%\n'\
+            f'<b>💿DISK:</b> {disk}%\n\n'\
+            f'<b>⚒️Physical Cores:</b> {cpu_count(logical=False)}\n'\
+            f'<b>🛠️Total Cores:</b> {cpu_count(logical=True)}\n\n'\
+            f'<b>✂️SWAP:</b> {get_readable_file_size(swap.total)} | <b>Used:</b> {swap.percent}%\n'\
+            f'<b>🖇️Memory Total:</b> {get_readable_file_size(memory.total)}\n'\
+            f'<b>📎Memory Free:</b> {get_readable_file_size(memory.available)}\n'\
+            f'<b>🔗Memory Used:</b> {get_readable_file_size(memory.used)}\n'
     sendMessage(stats, context.bot, update.message)
 
 
