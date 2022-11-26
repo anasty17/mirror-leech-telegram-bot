21ad8e30f555a8313b624cd43132140f8acff263
anasty17 <e.anastayyar@gmail.com>
2022-11-27 00:51:15 +0200

    fix rss unsuball - not deleting from database due to bot_id wasn't added
    Signed-off-by: anasty17 <e.anastayyar@gmail.com>

--- bot/helper/ext_utils/db_handler.py
+++ bot/helper/ext_utils/db_handler.py
@@ -152,7 +152,7 @@ class DbManger:
     def trunc_table(self, name):
         if self.__err:
             return
-        self.__db[name].drop()
+        self.__db[name][bot_id].drop()
         self.__conn.close()
 
 if DATABASE_URL:
