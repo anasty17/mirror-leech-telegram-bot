6da5c5a1329c1d8c4035f3734c2d0f334f8a76d2
pikaproject <125982436+pikaproject@users.noreply.github.com>
2023-02-21 22:58:25 +0800

    Update clone.py

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -106,7 +106,7 @@ async def cloneNode(client, message):
                     await update_all_messages()
             except IndexError:
                 pass
-        cc = f'\n\n<b>cc: </b>{tag}'
+        cc = f'\n\n<b>👤: </b>{tag}'
         if button in ["cancelled", ""]:
             await sendMessage(message, f"{tag} {result}")
         else:
