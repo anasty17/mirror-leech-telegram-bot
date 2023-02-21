efb9bb06d4c516819b2f5427640e7b2cbee8b4bc
pikaproject <125982436+pikaproject@users.noreply.github.com>
2023-02-21 21:03:15 +0800

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -30,18 +30,19 @@ async def stats(client, message):
     total, used, free, disk = disk_usage('/')
     swap = swap_memory()
     memory = virtual_memory()
-    stats = f'<b>Commit Date:</b> {last_commit}\n\n'\
+    stats = f'<b>╭─《🐱 BOT STATISTICS 🐱》</b>\n'\
+            f'<b> Commit Date:</b> {last_commit}\n'\
             f'<b>Bot Uptime:</b> {get_readable_time(time() - botStartTime)}\n'\
-            f'<b>OS Uptime:</b> {get_readable_time(time() - boot_time())}\n\n'\
+            f'<b>OS Uptime:</b> {get_readable_time(time() - boot_time())}\n'\
             f'<b>Total Disk Space:</b> {get_readable_file_size(total)}\n'\
-            f'<b>Used:</b> {get_readable_file_size(used)} | <b>Free:</b> {get_readable_file_size(free)}\n\n'\
+            f'<b>Used:</b> {get_readable_file_size(used)} | <b>Free:</b> {get_readable_file_size(free)}\n'\
             f'<b>Upload:</b> {get_readable_file_size(net_io_counters().bytes_sent)}\n'\
-            f'<b>Download:</b> {get_readable_file_size(net_io_counters().bytes_recv)}\n\n'\
+            f'<b>Download:</b> {get_readable_file_size(net_io_counters().bytes_recv)}\n'\
             f'<b>CPU:</b> {cpu_percent(interval=0.5)}%\n'\
             f'<b>RAM:</b> {memory.percent}%\n'\
-            f'<b>DISK:</b> {disk}%\n\n'\
+            f'<b>DISK:</b> {disk}%\n'\
             f'<b>Physical Cores:</b> {cpu_count(logical=False)}\n'\
-            f'<b>Total Cores:</b> {cpu_count(logical=True)}\n\n'\
+            f'<b>Total Cores:</b> {cpu_count(logical=True)}\n'\
             f'<b>SWAP:</b> {get_readable_file_size(swap.total)} | <b>Used:</b> {swap.percent}%\n'\
             f'<b>Memory Total:</b> {get_readable_file_size(memory.total)}\n'\
             f'<b>Memory Free:</b> {get_readable_file_size(memory.available)}\n'\
