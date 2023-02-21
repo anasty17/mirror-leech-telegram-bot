c49828fba706e7d4afc93c904afcfafd1fb3f1f4
pikaproject <125982436+pikaproject@users.noreply.github.com>
2023-02-22 02:55:26 +0800

    Update listener.py

--- bot/helper/listener.py
+++ bot/helper/listener.py
@@ -263,7 +263,7 @@ class MirrorLeechListener:
     async def onUploadComplete(self, link: str, size, files, folders, typ, name):
         if self.isSuperGroup and config_dict['INCOMPLETE_TASK_NOTIFIER'] and DATABASE_URL:
             await DbManger().rm_complete_task(self.message.link)
-        msg = f"<b>---《🐱 Pik4Bot 🐱》---</b>\n<b>Name: </b><code>{escape(name)}</code>\n<b>Size: </b>{size}"
+        msg = f"<b>_____《🐱 Pik4Bot 🐱》_____</b>\n<b>☞ Name: </b><code>{escape(name)}</code>\n<b>☞ Size: </b>{size}"
         if self.isLeech:
             msg += f'\n<b>☞Total Files: </b>{folders}'
             if typ != 0:
