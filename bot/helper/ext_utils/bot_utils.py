0633972e358e3e032d5b4a96e324b9aa62e88399
pikaproject <125982436+pikaproject@users.noreply.github.com>
2023-02-26 18:08:49 +0800

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -175,8 +175,8 @@ def get_readable_message():
                 up_speed += float(spd.split('K')[0]) * 1024
             elif 'M' in spd:
                 up_speed += float(spd.split('M')[0]) * 1048576
-    bmsg = f"🖥️<b>Cpu Usage:</b> {cpu_percent()}% \n💾<b>Disk Free:</b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}"
-    bmsg += f"\n🎮<b>Free Ram:</b> {virtual_memory().percent}% \n🟢<b>Bot Uptime:</b> {get_readable_time(time() - botStartTime)}"
+    bmsg = f"🖥️<b>CPU:</b> {cpu_percent()}% | 💾<b>DISK:</b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}"
+    bmsg += f"\n🎮<b>RAM:</b> {virtual_memory().percent}% | 🟢<b>UPTIME:</b> {get_readable_time(time() - botStartTime)}"
     bmsg += f"\n🔻<b>DL:</b> {get_readable_file_size(dl_speed)}/s | 🔺<b>UL:</b> {get_readable_file_size(up_speed)}/s"
     if STATUS_LIMIT and tasks > STATUS_LIMIT:
         msg += f"<b>Page:</b> {PAGE_NO}/{PAGES} | <b>Tasks:</b> {tasks}\n"
