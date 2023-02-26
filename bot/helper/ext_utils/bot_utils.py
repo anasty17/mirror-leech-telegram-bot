2729d0b31894d3000ead8fa85f1ef9d0fc6918cd
pikaproject <125982436+pikaproject@users.noreply.github.com>
2023-02-26 18:05:18 +0800

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -175,9 +175,9 @@ def get_readable_message():
                 up_speed += float(spd.split('K')[0]) * 1024
             elif 'M' in spd:
                 up_speed += float(spd.split('M')[0]) * 1048576
-    bmsg = f"<b>🖥️:</b> {cpu_percent()}% |<b>💾:</b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}"
-    bmsg += f"\n<b>🎮:</b> {virtual_memory().percent}% |<b>🟢:</b> {get_readable_time(time() - botStartTime)}"
-    bmsg += f"\n<b>🔻:</b> {get_readable_file_size(dl_speed)}/s |<b>🔺:</b> {get_readable_file_size(up_speed)}/s"
+    bmsg = f"🖥️<b>Cpu Usage:</b> {cpu_percent()}% \n💾<b>Disk Free:</b> {get_readable_file_size(disk_usage(DOWNLOAD_DIR).free)}"
+    bmsg += f"\n🎮<b>Free Ram:</b> {virtual_memory().percent}% \n🟢<b>Bot Uptime:</b> {get_readable_time(time() - botStartTime)}"
+    bmsg += f"\n🔻<b>DL:</b> {get_readable_file_size(dl_speed)}/s | 🔺<b>UL:</b> {get_readable_file_size(up_speed)}/s"
     if STATUS_LIMIT and tasks > STATUS_LIMIT:
         msg += f"<b>Page:</b> {PAGE_NO}/{PAGES} | <b>Tasks:</b> {tasks}\n"
         buttons = ButtonMaker()
