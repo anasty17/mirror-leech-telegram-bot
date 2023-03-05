ffe20e00308679d660586bbb1bdec2ebaa175dea
pikaproject <125982436+pikaproject@users.noreply.github.com>
2023-03-06 01:53:11 +0800

    Update tg_upload_status.py

--- bot/helper/mirror_utils/status_utils/tg_upload_status.py
+++ bot/helper/mirror_utils/status_utils/tg_upload_status.py
@@ -8,8 +8,7 @@ class TgUploadStatus:
         self.__size = size
         self.__gid = gid
         self.message = listener.message
-        self.source = self.__listener.source
-
+        
     def processed_bytes(self):
         return self.__obj.uploaded_bytes
 
