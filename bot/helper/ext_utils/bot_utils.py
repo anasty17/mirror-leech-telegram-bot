bc7c17a8c10bbf6c7d8b429c5d2af7c8151415b5
pikaproject <125982436+pikaproject@users.noreply.github.com>
2023-03-06 01:44:45 +0800

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -127,7 +127,7 @@ def get_readable_message():
     for index, download in enumerate(list(download_dict.values())[COUNT:], start=1):
         if download.message.chat.type.name in ['SUPERGROUP', 'CHANNEL']:
             msg += f"<b>_____《🐱 Pik4Bot 🐱》_____</b>"
-            msg += f"\n\n<b>☞ {download.status()}: </b>"
+            msg += f"\n\n<b>☞ <a href='{download.message.link}'>{download.status()}</a>: </b>"
         else:
             msg += f"<b>_____《🐱 Pik4Bot 🐱》_____</b>"
             msg += f"\n\n<b>☞ {download.status()}: </b>"
@@ -135,8 +135,6 @@ def get_readable_message():
         if download.status() not in [MirrorStatus.STATUS_SPLITTING, MirrorStatus.STATUS_SEEDING]:
             msg += f"\n<b>☞</b> {get_progress_bar_string(download)} {download.progress()}"
             msg += f"\n<b>☞ Processed:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
-            msg += f"\n<b>☞ Source</b>: {download.source}"
-           #msg += f"\n<b>Elapsed</b>: {get_readable_time(time() - download.startTime)} | <b>ETA:</b> {download.eta()}"
             msg += f"\n<b>☞ Speed:</b> {download.speed()} | <b>ETA:</b> {download.eta()}"
             if hasattr(download, 'seeders_num'):
                 try:
