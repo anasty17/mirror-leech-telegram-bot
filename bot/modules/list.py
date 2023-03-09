f0bf72785efaee6c421fdba71ae616d174569ace
pikaproject <125982436+pikaproject@users.noreply.github.com>
2023-03-09 08:33:23 +0800

    Update list.py

--- bot/modules/list.py
+++ bot/modules/list.py
@@ -11,26 +11,23 @@ from bot.helper.telegram_helper.button_build import ButtonMaker
 from bot.helper.ext_utils.bot_utils import sync_to_async, new_task
 
 
-async def list_buttons(client, message):
-    if len(message.text.split()) == 1:
-        return await sendMessage(message, 'Send a search key along with command')
-    user_id = message.from_user.id
+async def list_buttons(user_id, isRecursive=True):
     buttons = ButtonMaker()
-    buttons.ibutton("📁 Folders", f"list_types {user_id} folders")
-    buttons.ibutton("📄 Files", f"list_types {user_id} files")
-    buttons.ibutton("🗂️ Both", f"list_types {user_id} both")
-    buttons.ibutton("⛔ Cancel", f"list_types {user_id} cancel")
-    button = buttons.build_menu(2)
-    await sendMessage(message, 'Choose option to list.', button)
+    buttons.ibutton("Folders", f"list_types {user_id} folders {isRecursive}")
+    buttons.ibutton("Files", f"list_types {user_id} files {isRecursive}")
+    buttons.ibutton("Both", f"list_types {user_id} both {isRecursive}")
+    buttons.ibutton(f"Recursive: {isRecursive}", f"list_types {user_id} rec {isRecursive}")
+    buttons.ibutton("Cancel", f"list_types {user_id} cancel")
+    return buttons.build_menu(2)
 
-async def _list_drive(key, message, item_type):
+async def _list_drive(key, message, item_type, isRecursive):
     LOGGER.info(f"listing: {key}")
     gdrive = GoogleDriveHelper()
-    msg, button = await sync_to_async(gdrive.drive_list, key, isRecursive=True, itemType=item_type)
+    msg, button = await sync_to_async(gdrive.drive_list, key, isRecursive=isRecursive, itemType=item_type)
     if button:
         await editMessage(message, msg, button)
     else:
-        await editMessage(message, f'❌ No result found for <i>{key}</i>')
+        await editMessage(message, f'No result found for <i>{key}</i>')
 
 @new_task
 async def select_type(client, query):
@@ -39,15 +36,27 @@ async def select_type(client, query):
     key = message.reply_to_message.text.split(maxsplit=1)[1].strip()
     data = query.data.split()
     if user_id != int(data[1]):
-        return await query.answer(text="😡 Not Yours!", alert=True)
+        return await query.answer(text="Not Yours!", alert=True)
+    elif data[2] == 'rec':
+        await query.answer()
+        isRecursive = not bool(eval(data[3]))
+        buttons = await list_buttons(user_id, isRecursive)
+        return await editMessage(message, 'Choose list options:', buttons)
     elif data[2] == 'cancel':
         await query.answer()
-        return await editMessage(message, "⛔ list has been canceled!")
+        return await editMessage(message, "list has been canceled!")
     await query.answer()
     item_type = data[2]
-    await editMessage(message, f"<b>🔎 Searching for <i>{key}</i></b>")
-    await _list_drive(key, message, item_type)
+    isRecursive = eval(data[3])
+    await editMessage(message, f"<b>Searching for <i>{key}</i></b>")
+    await _list_drive(key, message, item_type, isRecursive)
 
+async def drive_list(client, message):
+    if len(message.text.split()) == 1:
+        return await sendMessage(message, 'Send a search key along with command')
+    user_id = message.from_user.id
+    buttons = await list_buttons(user_id)
+    await sendMessage(message, 'Choose list options:', buttons)
 
-bot.add_handler(MessageHandler(list_buttons, filters=command(BotCommands.ListCommand) & CustomFilters.authorized))
+bot.add_handler(MessageHandler(drive_list, filters=command(BotCommands.ListCommand) & CustomFilters.authorized))
 bot.add_handler(CallbackQueryHandler(select_type, filters=regex("^list_types")))
