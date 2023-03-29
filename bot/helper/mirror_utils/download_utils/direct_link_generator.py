2f9b1e526c95c219e97d01df66b97d6b3c733d1a
Jack <92579700+JohnWickKeanue@users.noreply.github.com>
2023-03-29 18:01:13 +0530

    Filepress fixed!

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -468,7 +468,7 @@ def filepress(url):
             'id': raw.path.split('/')[-1],
             'method': 'publicDownlaod',
             }
-        api = f'{raw.scheme}://api.{raw.hostname}/api/file/downlaod/'
+        api = f'{raw.scheme}://{raw.hostname}/api/file/downlaod/'
         res = cget('POST', api, headers={'Referer': f'{raw.scheme}://{raw.hostname}'}, json=json_data).json()
     except Exception as e:
         raise DirectDownloadLinkException(f'ERROR: {e.__class__.__name__}')
