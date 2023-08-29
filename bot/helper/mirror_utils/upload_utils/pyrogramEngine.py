27a1aae57685df39c72535ae18d1f86f850afbd6
Shadow Hunter <139977665+ShadowHunterV@users.noreply.github.com>
2023-08-29 16:00:47 +0530

    Fix

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -15,7 +15,7 @@ from aioshutil import copy
 from bot import config_dict, GLOBAL_EXTENSION_FILTER, bot, user, IS_PREMIUM_USER
 from bot.helper.ext_utils.fs_utils import clean_unwanted, is_archive, get_base_name
 from bot.helper.ext_utils.bot_utils import sync_to_async
-from bot.helper.ext_utils.leech_utils import get_media_info, get_document_type, take_ss, get_audio_thumb, , convert_to_mp4
+from bot.helper.ext_utils.leech_utils import get_media_info, get_document_type, take_ss, get_audio_thumb, convert_to_mp4
 from bot.helper.telegram_helper.message_utils import deleteMessage
 
 LOGGER = getLogger(__name__)
