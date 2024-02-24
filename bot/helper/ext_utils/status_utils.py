55327eaf2afda7e0e0daaa893f800d4807287151
TreNor's .IT <152026909+startfix@users.noreply.github.com>
2024-02-24 23:52:30 +0700

    Update status_utils.py

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -112,8 +112,8 @@ def get_progress_bar_string(pct):
     pct = float(pct.strip("%"))
     p = min(max(pct, 0), 100)
     cFull = int(p // 8)
-    p_str = "■" * cFull
-    p_str += "□" * (12 - cFull)
+    p_str = "●" * cFull
+    p_str += "○" * (12 - cFull)
     return f"[{p_str}]"
 
 
