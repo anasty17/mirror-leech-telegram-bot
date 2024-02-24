0ccb0c73aa80cc1c8dcfb302bf5afda95314fdae
TreNor's .IT <152026909+startfix@users.noreply.github.com>
2024-02-25 00:05:10 +0700

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -1,19 +1,20 @@
+
 from bot import CMD_SUFFIX
 
 
 class _BotCommands:
     def __init__(self):
         self.StartCommand = f"start{CMD_SUFFIX}"
-        self.MirrorCommand = [f"mirror{CMD_SUFFIX}", f"m{CMD_SUFFIX}"]
-        self.QbMirrorCommand = [f"qbmirror{CMD_SUFFIX}", f"qm{CMD_SUFFIX}"]
-        self.JdMirrorCommand = [f"jdmirror{CMD_SUFFIX}", f"jm{CMD_SUFFIX}"]
-        self.YtdlCommand = [f"ytdl{CMD_SUFFIX}", f"y{CMD_SUFFIX}"]
-        self.LeechCommand = [f"leech{CMD_SUFFIX}", f"l{CMD_SUFFIX}"]
-        self.QbLeechCommand = [f"qbleech{CMD_SUFFIX}", f"ql{CMD_SUFFIX}"]
-        self.JdLeechCommand = [f"jdLeech{CMD_SUFFIX}", f"jl{CMD_SUFFIX}"]
-        self.YtdlLeechCommand = [f"ytdlleech{CMD_SUFFIX}", f"yl{CMD_SUFFIX}"]
-        self.CloneCommand = f"clone{CMD_SUFFIX}"
-        self.CountCommand = f"count{CMD_SUFFIX}"
+        self.MirrorCommand = [f"mirror1{CMD_SUFFIX}", f"m1{CMD_SUFFIX}"]
+        self.QbMirrorCommand = [f"qbmirror1{CMD_SUFFIX}", f"qm1{CMD_SUFFIX}"]
+        self.JdMirrorCommand = [f"jdmirror1{CMD_SUFFIX}", f"jm1{CMD_SUFFIX}"]
+        self.YtdlCommand = [f"ytdl1{CMD_SUFFIX}", f"y1{CMD_SUFFIX}"]
+        self.LeechCommand = [f"leech1{CMD_SUFFIX}", f"l1{CMD_SUFFIX}"]
+        self.QbLeechCommand = [f"qbleech1{CMD_SUFFIX}", f"ql1{CMD_SUFFIX}"]
+        self.JdLeechCommand = [f"jdLeech1{CMD_SUFFIX}", f"jl1{CMD_SUFFIX}"]
+        self.YtdlLeechCommand = [f"ytdlleech1{CMD_SUFFIX}", f"yl1{CMD_SUFFIX}"]
+        self.CloneCommand = f"clone1{CMD_SUFFIX}"
+        self.CountCommand = f"count1{CMD_SUFFIX}"
         self.DeleteCommand = f"del{CMD_SUFFIX}"
         self.CancelTaskCommand = [f"cancel{CMD_SUFFIX}", f"c{CMD_SUFFIX}"]
         self.CancelAllCommand = f"cancelall{CMD_SUFFIX}"
@@ -27,7 +28,7 @@ class _BotCommands:
         self.AddSudoCommand = f"addsudo{CMD_SUFFIX}"
         self.RmSudoCommand = f"rmsudo{CMD_SUFFIX}"
         self.PingCommand = f"ping{CMD_SUFFIX}"
-        self.RestartCommand = f"restart{CMD_SUFFIX}"
+        self.RestartCommand = f"restart1{CMD_SUFFIX}"
         self.StatsCommand = f"stats{CMD_SUFFIX}"
         self.HelpCommand = f"help{CMD_SUFFIX}"
         self.LogCommand = f"log{CMD_SUFFIX}"
@@ -35,7 +36,7 @@ class _BotCommands:
         self.AExecCommand = f"aexec{CMD_SUFFIX}"
         self.ExecCommand = f"exec{CMD_SUFFIX}"
         self.ClearLocalsCommand = f"clearlocals{CMD_SUFFIX}"
-        self.BotSetCommand = [f"bsetting{CMD_SUFFIX}", f"bs{CMD_SUFFIX}"]
+        self.BotSetCommand = [f"bsetting1{CMD_SUFFIX}", f"bs1{CMD_SUFFIX}"]
         self.UserSetCommand = [f"usetting{CMD_SUFFIX}", f"us{CMD_SUFFIX}"]
         self.BtSelectCommand = f"btsel{CMD_SUFFIX}"
         self.RssCommand = f"rss{CMD_SUFFIX}"
