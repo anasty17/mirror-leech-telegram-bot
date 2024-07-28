ce8c9c8753d4f3bb0b23ccfa5c6c839ad6bee935
Zuko <tansautn@gmail.com>
2024-07-28 13:41:05 +0700

    Add crypt env

---
+++ crypt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+TASK=$1
+if [ "$TASK" = "encrypt"  ]; then
+  echo "encrypting config.env with cipher aes-256-cbc"
+  openssl aes-256-cbc -a -salt -pbkdf2 -in config.env -out config_env.enc
+fi
+if [ "$TASK" = "encrypt"  ]; then
+  echo "backing up config.env"
+  cp config.env config_env.bak
+  echo "decrypting config.env with cipher aes-256-cbc"
+  openssl aes-256-cbc -d -a -pbkdf2 -in config_env.enc -out config.env
+fi
--- start.sh
+++ start.sh
@@ -1,1 +1,11 @@
+#TODO: check to use update or not
 python3 update.py && python3 -m bot
+
+
+#OLD from debian deploy
+#sudo kill $(pgrep -f "/var/www/ml/venv/bin/python3 /var/www/ml/venv/bin/gunicorn")
+#source ./venv/bin/activate
+#python3 -m bot
+#!/bin/bash
+#source /www/ml/venv/bin/activate
+#nohup python3 -m bot &
