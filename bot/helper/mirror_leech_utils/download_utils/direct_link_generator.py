a7ace76a1723127e6863607f4ed1f2414632e3a6
White Demon <133562822+whitedemon938@users.noreply.github.com>
2025-03-03 23:53:22 +0530

    Update direct_link_generator.py

--- bot/helper/mirror_leech_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_leech_utils/download_utils/direct_link_generator.py
@@ -319,7 +319,8 @@ def devuploads(url):
     if not randpost:
         raise DirectDownloadLinkException("ERROR: Unable to find xd value")
     data['xd'] = randpost.text.strip()
-    res = session.post(url, data=data)
+    proxy = "http://hsakalu2:hsakalu2@45.151.162.198:6600"
+    res = session.post(url, data=data, proxies={'http': proxy, 'https': proxy})
     html = HTML(res.text)
     if not html.xpath("//input[@name='orilink']/@value"):
         raise DirectDownloadLinkException("ERROR: Unable to find Direct Link")
