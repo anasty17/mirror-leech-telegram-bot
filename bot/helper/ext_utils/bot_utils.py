8594ef0224a7196f05b177d1208f3c68d12a3bc8
anasty17 <e.anastayyar@gmail.com>
2025-05-21 20:49:08 +0300

    minor fix
    Signed-off-by: anasty17 <e.anastayyar@gmail.com>

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -151,11 +151,10 @@ def arg_parser(items, arg_base):
                         if not sub_list:
                             break
                         check = " ".join(sub_list).strip()
-                        if part != "-ff":
-                            break
-                        if check.startswith("[") and check.endswith("]"):
-                            break
-                        elif not check.startswith("["):
+                        if part == "-ff":
+                            if check.startswith("[") and check.endswith("]"):
+                                break
+                        else:
                             break
                     sub_list.append(items[j])
                 if sub_list:
